522eb4eb508588e092df04d70a53f002bd757b15
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-01-28 09:12:41 -0800

    capitalize, reverse, calculator methods added

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+const capitalize = string => {
+    return string[0].toUpperCase() + string.substr(1, string.length);
+};
+
+const reverseString = string => {
+    return string.split('').reverse().join('');
+};
+
+const calculator = {
+    add(a, b) {
+        return a + b;
+    },
+
+    subtract(a, b) {
+        return a - b;
+    },
+
+    divide(a, b) {
+        return a / b;
+    },
+
+    multiply(a, b) {
+        return a * b;
+    }
+};
