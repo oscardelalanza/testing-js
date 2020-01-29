d559bdc4795987c6882234dad7906a2595030133
webmarkyn <web.markyn@gmail.com>
2020-01-29 17:00:51 +0300

    added caesar func and tests

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,23 @@ const reverseString = string => {
     return string.split('').reverse().join('');
 };
 
+const caesar = (string) => {
+    string = string.split('')
+    for (let i=0;i<string.length;i++) {
+        if (string[i].match(/\w/)) {
+            let charCode = string[i].charCodeAt(0)+3
+            if (charCode > 90 && charCode < 97) {
+              charCode = 65 + (charCode - 90)-1
+            }
+            if (charCode > 122) {
+              charCode = 97 + (charCode-122)-1
+            }
+            string[i] = String.fromCharCode(charCode)
+        }
+    }
+    return string.join('')
+}
+
 class Calculator {
     constructor(a, b) {
         this.add = a + b;
@@ -18,5 +35,6 @@ class Calculator {
 export {
     capitalize,
     reverseString,
-    Calculator
+    Calculator,
+    caesar
 };
--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,27 +1,41 @@
-import { capitalize, reverseString, Calculator } from '../js';
+import { capitalize, reverseString, caesar, Calculator } from "../js";
 
 const calculator = new Calculator(10, 2);
 
-test('Takes a string and returns that with first character capitalized', () => {
-    expect(capitalize('my string')).toBe('My string');
+test("Takes a string and returns that with first character capitalized", () => {
+  expect(capitalize("my string")).toBe("My string");
 });
 
-test('Takes a string and returns it reversed', () => {
-    expect(reverseString('mystring')).toBe('gnirtsym');
+test("Takes a string and returns it reversed", () => {
+  expect(reverseString("mystring")).toBe("gnirtsym");
 });
 
-test('Calculator add', ()=> {
-    expect(calculator.add).toBe(12);
+test("Encodes the string using caesar cypher", () => {
+  expect(
+    caesar(
+    `A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R,
+    S, T, U, V, X, Y, Z, a, b, c, d, e, f, g, h, i, j, k,
+    l, m, n, o, p, q, r, s, t, u v, x, y, z.!`
+    )
+  ).toBe(
+    `D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U,
+    V, W, X, Y, A, B, C, d, e, f, g, h, i, j, k, l, m, n,
+    o, p, q, r, s, t, u, v, w, x y, a, b, c.!`
+  );
 });
 
-test('Calculator subtract', ()=> {
-    expect(calculator.subtract).toBe(8);
+test("Calculator add", () => {
+  expect(calculator.add).toBe(12);
 });
 
-test('Calculator divide', ()=> {
-    expect(calculator.divide).toBe(5);
+test("Calculator subtract", () => {
+  expect(calculator.subtract).toBe(8);
 });
 
-test('Calculator multiply', ()=> {
-    expect(calculator.multiply).toBe(20);
+test("Calculator divide", () => {
+  expect(calculator.divide).toBe(5);
+});
+
+test("Calculator multiply", () => {
+  expect(calculator.multiply).toBe(20);
 });
